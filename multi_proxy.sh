67540eb96eeb589854be4074f0213e142f4a241d
jsz <jszpilewski@me.com>
2009-12-27 15:28:36 +0100

    multi proxuy

--- multi_proxy.sh
+++ multi_proxy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-ssh -t -L 8887:localhost:8887 root@superking.org ssh -t -g -D 8887 root@funblog.me
+ssh -t -L 8887:localhost:8886 root@superking.org ssh -t -L 8886:localhost:8885 root@funblog.me ssh -t -g -D 8885 root@superking.org
+
+
+#ssh -t -L 8887:localhost:8887 root@superking.org ssh -t -D 8887 root@funblog.me
 
 #ssh -g -D 8887 root@funblog.me
